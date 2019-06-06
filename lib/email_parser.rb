43cfeaed4b6e2e6310a8f9e10950b5625cff7dbf
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-06 17:24:05 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,7 +15,7 @@ end
 def parse(emails)
 
 
-  emails_array = emails.split(",")
+  emails_array = emails.split(/[, ]).uniq
 
 #emails_array = emails.split(/[,?\s]/).uniq
   #emails_array = emails.split(/[, ]/).uniq
